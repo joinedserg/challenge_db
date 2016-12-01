f12b5e99d854ae460566cf58d63aefff9032f034
natalya-erina <natalya.erina.96@gmail.com>
2016-12-01 11:24:42 +0300

    Changed user entity

--- src/main/java/dev/models/User.java
+++ src/main/java/dev/models/User.java
@@ -1,88 +1,94 @@
 package dev.models;
 
-import dev.models.simpleentity.*;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.SecondaryTable;
 import javax.persistence.Table;
+import org.hibernate.annotations.WhereJoinTable;
 
 
 
 @Entity
 @Table(name = "entities")
+@SecondaryTable(name="values")
 public class User {
     //static Integer entity_type_id = 0;
     
     public User() {
-this.entity_id = 12;
     }
 
     public User(String name, String surname) {
-        this.name = new Name(name);
-            //this.name.setUser(this);
-            //this.surname = new Surname(surname);
+        this.name = name;
+        this.surname = surname;
     }
     
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "entity_id", unique = true, nullable = false)
     private Integer entity_id;
     
-    @MapsId("entity_id") 
-    @OneToOne(cascade=CascadeType.ALL)
-    //@JoinColumn(name="name_fk")
-    //@MapsId("entity_id")
-    private Name name;
+    
+    
+    @JoinTable(name="values",
+            joinColumns={@JoinColumn(name = "entity_id")},
+            inverseJoinColumns = @JoinColumn(name = "entity_id")
+    )
+    @WhereJoinTable(clause = "attribute_id = 0")
+    @Column(table="values", name="text_value")
+    private String name;
+   
+    
+    
+    @JoinTable(name="values",
+            joinColumns = @JoinColumn(name="entity_id"),
+            inverseJoinColumns = @JoinColumn(name="entity_id")
+    )
+    @WhereJoinTable(clause = "attribute_id = 1")
+    @Column(name="text_value")
+    private String surname;
+    
+    
+    @JoinTable(name="values",
+            joinColumns=@JoinColumn(name = "entity_id"),
+            inverseJoinColumns = @JoinColumn(name = "entity_id")
+    )
+    @Column(table="values", name="attribute_id")
+    private Integer name_attribute_id = 0;
+    
+    @JoinTable(name="values",
+    joinColumns=@JoinColumn(name = "entity_id"),
+    inverseJoinColumns = @JoinColumn(name = "entity_id")
+    )
+    @Column(table="values", name="attribute_id")
+    private Integer surname_attribute_id = 1;
 
-    //@OneToOne(mappedBy="user", cascade=CascadeType.ALL)
-    //private Surname surname;
-
-//    public Surname getSurname() {
-//        return surname;
-//    }
-//
-//    public void setSurname(Surname surname) {
-//        this.surname = surname;
-//    }
-    
-    public Integer getId() {
-        return entity_id;
-    }
-
-    public void setId(Integer id) {
-        this.entity_id = id;
-    }
-
-    
-    
-    
-    
-    /*@OneToOne
-    @JoinColumn(name="id_fk")
-    private Surname surname;*/
-    
-    public Name getName() {
-        return name;
-    }
-
-    public void setName(Name name) {
-        this.name = name;
         
-        this.name.setId(new NamePK(entity_id, 1));
-    }    
-
-    /*public Surname getSurname() {
+    public String getSurname() {
         return surname;
     }
 
-    public void setSurname(Surname surname) {
+    public void setSurname(String surname) {        
         this.surname = surname;
-    }*/
+    }
+
+    public Integer getEntity_id() {
+        return entity_id;
+    }
+
+    public void setEntity_id(Integer entity_id) {
+        this.entity_id = entity_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
