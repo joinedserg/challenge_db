87c82aa1867f9a884b59d95f40223b38783a10e7
serg <joinerserg@gmail.com>
2016-12-04 22:43:59 +0300

    Изменения, которые будут включены в коммит: 	изменено:      src/main/java/dev/models/User.java

--- src/main/java/dev/models/User.java
+++ src/main/java/dev/models/User.java
@@ -14,37 +14,31 @@ import javax.persistence.*;
 public class User {
     
     public User() {
-    	//this.attributes = new HashMap<String, Attribute>();
+    	this.attributes = new HashMap<String, Attribute>();
     }
     
 	public User(String name, String surname) {
-		
+		/*
 		attribute = new ArrayList<Attribute>();
 		
 		attribute.add(new Attribute(0, name));
-		attribute.add(new Attribute(1, surname));
+		attribute.add(new Attribute(1, surname));*/
 		
 		
-		//this.attributes = new HashMap<String, Attribute>();
+		this.attributes = new HashMap<String, Attribute>();
     	
-    	//attributes.put("name", new Attribute(0, name));
-    	//attributes.put("surname", new Attribute(1, surname));
+    	attributes.put("name", new Attribute(0, name));
+    	attributes.put("surname", new Attribute(1, surname));
 		
     }
 	
+	private Integer entity_id;
+    private Map<String, Attribute> attributes;
+    
+    
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "entity_id")    
-    private Integer entity_id;
-    //private Map<String, Attribute> attributes;
-    
-	
-	@OneToMany(fetch=FetchType.EAGER,
-			cascade = CascadeType.ALL)
-	@JoinColumn(name="entity_id")
-	List<Attribute> attribute;
-    
-    
     public Integer getEntity_id() {
 		return entity_id;
 	}
@@ -53,15 +47,18 @@ public class User {
 		this.entity_id = entity_id;
 	}
     
+	@OneToMany(fetch=FetchType.EAGER,
+			cascade = CascadeType.ALL)
+	@JoinColumn(name="entity_id")
 	public List<Attribute> getAttr() {
-		//return new ArrayList(attributes.values());
-		return attribute;
+		return new ArrayList(attributes.values());
+		//return attribute;
 		//return null;
 	}
 	
 	public void setAttr(List<Attribute> list) {
 		//??
-		this.attribute = list;
+		//this.attribute = list;
 		System.out.println("setAttr");
 		
 	}
